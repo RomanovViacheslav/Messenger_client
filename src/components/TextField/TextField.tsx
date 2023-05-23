138acf8339d0adca6a62a6aab2abdaa40b2ae106
Viacheslav Romanov <kozyr52@gmail.com>
2023-05-23 04:22:57 +0300

    userAgent reduxThunk

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -9,24 +9,21 @@ import {
 } from './TextField.styled';
 
 export const TextField = memo(
-  ({ label, placeholder, value, onChange, errorText, type, error, ...props }: TextFieldProps) => {
-    console.log('textfield render');
-    return (
-      <StyledFormControl variant="standard">
-        <StyledInputLabel shrink htmlFor={label}>
-          {label}
-        </StyledInputLabel>
-        <StyledInput
-          placeholder={placeholder}
-          id={label}
-          type={type}
-          value={value}
-          onChange={onChange}
-          error={error}
-          {...props}
-        />
-        {error && <StyledFormHelperText error>{errorText}</StyledFormHelperText>}
-      </StyledFormControl>
-    );
-  },
+  ({ label, placeholder, value, onChange, errorText, type, error, ...props }: TextFieldProps) => (
+    <StyledFormControl variant="standard">
+      <StyledInputLabel shrink htmlFor={label}>
+        {label}
+      </StyledInputLabel>
+      <StyledInput
+        placeholder={placeholder}
+        id={label}
+        type={type}
+        value={value}
+        onChange={onChange}
+        error={error}
+        {...props}
+      />
+      {error && <StyledFormHelperText error>{errorText}</StyledFormHelperText>}
+    </StyledFormControl>
+  ),
 );
